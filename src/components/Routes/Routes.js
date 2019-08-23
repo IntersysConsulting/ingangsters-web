d01dd154ff1ea026e513d9867da1fb43a5040cd1
Andres Hernandez <ahernandez@intersysconsulting.com>
2019-08-23 12:30:28 -0500

    Creates Routes component - Creates a blank component for Signin, Signup, Home, Checkout and Cart

--- src/components/Routes/Routes.js
+++ src/components/Routes/Routes.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import Home from "../Home/Home";
+import Signin from "../Sign/Signin";
+import SigninAdmin from "../Sign/SigninAdmin";
+import Signup from "../Sign/Signup";
+import Checkout from "../Checkout/Checkout";
+import Cart from "../Cart/Cart";
 
-const Routes = () => {
-  return <div />;
-};
+function Routes() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route path="/" exact component={Home} />
+        <Route path="/signin" exact component={Signin} />
+        <Route path="/signin/admin" exact component={SigninAdmin} />
+        <Route path="/signup" exact component={Signup} />
+        <Route path="/checkout" exact component={Checkout} />
+        <Route path="/cart" exact component={Cart} />
+      </Switch>
+    </BrowserRouter>
+  );
+}
 
 export default Routes;
---
+++ src/components/Sign/SigninAdmin.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SigninAdmin = () => {
+  return (
+    <div>
+      <h1>Sign in for Admis</h1>
+    </div>
+  );
+};
+
+export default SigninAdmin;
