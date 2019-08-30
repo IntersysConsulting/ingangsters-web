0f3f8287104c38bb5a00425af58eced023816830
Sofia Robles <srobles@intersysconsulting.com>
2019-08-30 08:55:02 -0500

    Modified home to leave it as it was before testing the Simple NavBar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,8 @@
 import React from "react";
-import SimpleNavBar from "../SimpleNavBar/SimpleNavBar"
+
 const Home = () => {
   return (
     <div>
-      <SimpleNavBar/>
       <h1>Home</h1>
     </div>
   );
