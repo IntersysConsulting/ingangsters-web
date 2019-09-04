2542c639d91d1a6aee294f0ff0612482a253b232
Andres Hernandez <ahernandez@intersysconsulting.com>
2019-09-04 13:52:44 -0500

    WIP: login component

--- src/components/SimpleNavBar/SimpleNavBar.js
+++ src/components/SimpleNavBar/SimpleNavBar.js
@@ -1,25 +1,31 @@
-import React, {Component} from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import NavbarBrand from 'react-bootstrap/NavbarBrand'
-import './SimpleNavBar.css';
+import React, { Component } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import NavbarBrand from "react-bootstrap/NavbarBrand";
+import "./SimpleNavBar.css";
 
-class Navigation extends Component{
-    render(){
-        return(  
-                <Navbar bg="dark" variant="dark" fixedTop sticky="top"  className="justify-content-center navbar">
-                    <center>
-                    <NavbarBrand href="/" className="navbar-brand">      
-                        <img
-                            alt=""
-                            src="/assets/logo.png"
-                            width="75"
-                            height="75"
-                            className="mx-auto"
-                        />
-                    </NavbarBrand>
-                    </center>            
-                </Navbar>
-        )
-    }
+class SimpleNavBar extends Component {
+  render() {
+    return (
+      <Navbar
+        bg="dark"
+        variant="dark"
+        fixedTop
+        sticky="top"
+        className="justify-content-center navbar"
+      >
+        <center>
+          <NavbarBrand href="/" className="navbar-brand">
+            <img
+              alt=""
+              src="/assets/logo.png"
+              width="75"
+              height="75"
+              className="mx-auto"
+            />
+          </NavbarBrand>
+        </center>
+      </Navbar>
+    );
+  }
 }
-export default Navigation;
+export default SimpleNavBar;
