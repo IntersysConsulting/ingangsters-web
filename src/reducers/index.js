a24d0ecebb929e4f4f30dcd70f1f31af42ea94c7
Jorge Garcia <jogadev@outlook.com>
2019-09-06 10:15:01 -0500

    Removed unused state objects and fixed icon

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -11,27 +11,6 @@ import shadow from "./shadow";
 const reducerCombined = combineReducers({ sample, navBar, shadow });
 
 export const initialState = {
-  userData: {
-    name: "John Doe",
-    id: "3312"
-  },
-
-  cartData: {
-    id: "3399",
-    items: [],
-    createdAt: "TODAY",
-    updatedAt: "NOW"
-  },
-
-  session: {
-    token: "ABC"
-  },
-
-  sample: {
-    myNumber: 10,
-    myString: "Hola mundo!!!!"
-  },
-
   navBar: {
     mouseOverAccountButton: false,
     mouseOverCartButton: false,
