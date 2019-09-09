47b30d464df640e9d0444e491b308f6b2280d959
Sofia Robles <srobles@intersysconsulting.com>
2019-09-09 08:45:21 -0500

    Begining Files of Admin Tab Bar

---
+++ src/components/AdminTabBar/AdminTabBar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./AdminTabBar.css";
+import "../../css/colors.css";
+
+class AdminTabBar extends Component {
+  render() {
+    return <div></div>;
+  }
+}
+export default AdminTabBar;
