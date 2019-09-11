6f833d679ac84b77c473e80cf52dfd58744a6ed1
Jorge Garcia <jogadev@outlook.com>
2019-09-11 16:38:49 -0500

    Missed removing unnecesary hardcoded values

--- src/reducers/adminProducts.js
+++ src/reducers/adminProducts.js
@@ -1,6 +1,5 @@
 import * as types from "../actions/types/adminProducts";
 
-<<<<<<< HEAD
 const initialState = {
   list: [],
   fetching: false,
@@ -8,15 +7,11 @@ const initialState = {
 };
 
 export default (state = initialState, action) => {
-=======
-export default (state = {}, action) => {
->>>>>>> d09192a970245d198952305f96d67842e8bcb2b1
   switch (action.type) {
     case types.UPDATE_PRODUCTS:
       return Object.assign({}, state, { list: action.newProductList });
 
     case types.FETCHING_PRODUCTS:
-<<<<<<< HEAD
       console.log("Fetch start");
       return Object.assign({}, state, { fetching: true });
 
@@ -26,13 +21,6 @@ export default (state = {}, action) => {
         fetching: false,
         finishedFetching: true
       });
-=======
-      return Object.assign({}, state, { fetching: true });
-
-    case types.FINISHED_FETCHING_PRODUCTS:
-      console.log("FINISH");
-      return Object.assign({}, state, { fetching: false });
->>>>>>> d09192a970245d198952305f96d67842e8bcb2b1
 
     default:
       return state;
