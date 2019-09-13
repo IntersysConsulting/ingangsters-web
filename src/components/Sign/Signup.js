8fafc5fadfaee0b72226d018f31bf69eba113cad
{Sergio Rodriguez} <{arodriguezloza@yahoo.com.mx}>
2019-09-13 16:39:06 -0500

    implementations of validations in the form

--- src/components/Sign/Signup.js
+++ src/components/Sign/Signup.js
@@ -6,7 +6,9 @@ import "../../css/colors.css";
 import { FaEye, FaCentercode } from "react-icons/fa";
 import { connect } from "react-redux";
 import { login } from "../../actions/creators/auth";
-import { Form, Row, Col, InputGroup, Button } from "react-bootstrap";
+import { Form, Row, Col, InputGroup, Button, FormCheck } from "react-bootstrap";
+import { Formik, FormikProps, Field, ErrorMessage } from "formik";
+import * as yup from "yup";
 
 const Login = ({ login, isAuthenticated }) => {
   const [values, setValues] = useState({
@@ -18,7 +20,7 @@ const Login = ({ login, isAuthenticated }) => {
   const [showPassword, setShowPassword] = useState(false);
   const [showPasswordConfirm, setShowPasswordConfirm] = useState(false);
 
-  const {
+/*   const {
     firstName,
     lastName,
     phone,
@@ -28,6 +30,7 @@ const Login = ({ login, isAuthenticated }) => {
   } = values;
 
   const handleChange = name => event => {
+    console.log("Hola Mundo")
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
@@ -48,7 +51,7 @@ const Login = ({ login, isAuthenticated }) => {
 
   if (isAuthenticated) {
     return <Redirect to="/" />;
-  }
+  } */
 
   const toggleShow = () => {
     setShowPassword(!showPassword);
@@ -56,6 +59,25 @@ const Login = ({ login, isAuthenticated }) => {
   const toggleShowConfirm = () => {
     setShowPasswordConfirm(!showPasswordConfirm);
   };
+
+  const schema = yup.object({
+    firstName: yup.string().required(),
+    lastName: yup.string().required(),
+    phone: yup
+      .number()
+      .integer()
+      .positive(),
+    email: yup.string().email(),
+    password: yup
+      .string()
+      .min(8)
+      .required(),
+    passwordConfirm: yup
+      .string()
+      .min(8)
+      .required(),
+    terms: yup.bool().required()
+  });
 
   const signUpForm = () => (
     /* <div className="container mt-5">
@@ -159,7 +181,7 @@ const Login = ({ login, isAuthenticated }) => {
       </div>
     </div> */
 
-    <div className="container mt-5">
+    /*  <div className="container mt-5">
       <div className="row login-form">
         <div className="col-12">
           <h2>Sign up</h2>
@@ -199,26 +221,47 @@ const Login = ({ login, isAuthenticated }) => {
                   Please provide a valid phone.
                 </Form.Control.Feedback>
               </Form.Group>
-
               <Form.Group as={Col} md="12" controlId="validationPassword">
                 <Form.Label>Password</Form.Label>
-
                 <InputGroup>
-                  
                   <Form.Control
                     required
                     onChange={handleChange("password")}
                     type={showPassword ? "text" : "password"}
                     placeholder="password"
-                    className="form-control"
                     value={password}
                   />
                   <InputGroup.Prepend>
-                    {/* <InputGroup.Text id="inputGroupPrepend">@</InputGroup.Text> */}
-                    <div className="col-1 text-center" onClick={toggleShowConfirm}>
-                  <FaEye size={32} />
-                </div>
-
+                    <div className="col-1 text-center" onClick={toggleShow} ref={hola}>
+                      <FaEye size={32} />
+                    </div>
+                  </InputGroup.Prepend>
+                  <Form.Control.Feedback as={Col} md="12" type="invalid">
+                    Please provide a password.
+                  </Form.Control.Feedback>
+                </InputGroup>
+              </Form.Group>
+              <Form.Group
+                as={Col}
+                md="12"
+                controlId="validationConfirmPassword"
+              >
+                <Form.Label>Confirm Password</Form.Label>
+                <InputGroup>
+                  <Form.Control
+                    required
+                    onChange={handleChange("passwordConfirm")}
+                    type={showPasswordConfirm ? "text" : "password"}
+                    placeholder="confirm password"
+                    value={passwordConfirm}
+                  />
+                  <InputGroup.Prepend>
+                    <div
+                      className="col-1 text-center"
+                      onClick={toggleShowConfirm}
+                    >
+                      <FaEye size={32} />
+                    </div>
                   </InputGroup.Prepend>
                   <Form.Control.Feedback as={Col} md="12" type="invalid">
                     Please provide a password.
@@ -228,16 +271,208 @@ const Login = ({ login, isAuthenticated }) => {
             </Form.Row>
             <Form.Group>
               <Form.Check
+                type="checkbox"
                 required
-                label="Agree to terms and conditions"
                 feedback="You must agree before submitting."
-              />
+                label={
+                  <React.Fragment>
+                    I accept the {'\u00a0'}
+                    <Link className="link" to="#">
+                    Terms of Use
+                    </Link>
+                    {'\u00a0'} & {'\u00a0'}
+                    <Link className="link" to="#">
+                      Privacy Policy
+                    </Link>
+                    .
+                  </React.Fragment>
+                }
+              ></Form.Check>
             </Form.Group>
             <Button type="submit">Submit form</Button>
           </Form>
         </div>
       </div>
-    </div>
+    </div> */
+
+    <Formik
+      validationSchema={schema}
+      onSubmit={console.log}
+      initialValues={{
+        firstName: "",
+        lastName: "",
+        email:"",
+        phone:""
+      }}
+    >
+      {({
+        handleSubmit,
+        handleChange,
+        handleBlur,
+        values,
+        touched,
+        isValid,
+        errors
+      }) => (
+        <div className="container mt-5">
+          <div className="row login-form">
+            <div className="col-12">
+              <h2>Sign up</h2>
+              <h5>Please fill this form to create an account! </h5>
+              <hr />
+              <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                <Form.Row>
+                  <Form.Group as={Col} md="6" controlId="validationFirstName">
+                    <Form.Label>First name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      name ="firstName"
+                      placeholder="First name"
+                      value = {values.firstName}
+                      onChange={handleChange}
+                      isValid={touched.firstName && !errors.firstName}
+                      isInvalid = {!!errors.firstName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide your first name.
+                      {/* {errors.firstName} */}
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="6" controlId="validationLastName">
+                    <Form.Label>Last name</Form.Label>
+                    <Form.Control
+                      required
+                      type="text"
+                      name ="lastName"
+                      placeholder="Last name"
+                      value = {values.lastName}
+                      onChange={handleChange}
+                      isValid={touched.lastName && !errors.lastName}
+                      isInvalid = {!!errors.lastName}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide your last name.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="12" controlId="validationEmail">
+                    <Form.Label>Email</Form.Label>
+                    <Form.Control 
+                    required
+                    type="email"
+                    name ="email"
+                    placeholder="email"
+                    value = {values.email}
+                    onChange={handleChange}
+                    isValid={touched.email && !errors.email}
+                    isInvalid = {!!errors.email}
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid email.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="12" controlId="validationPhone">
+                    <Form.Label>Phone</Form.Label>
+                    <Form.Control
+                      type="tel"
+                      pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}"
+                      required
+                      name ="phone"
+                      placeholder="Phone (optional)"
+                      value = {values.phone}
+                      onChange={handleChange}
+                      isValid={touched.phone && !errors.phone}
+                      isInvalid = {!!errors.phone}
+
+                    />
+                    <Form.Control.Feedback type="invalid">
+                      Please provide a valid phone.
+                    </Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group as={Col} md="12" controlId="validationPassword">
+                    <Form.Label>Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        required
+                        type={showPassword ? "text" : "password"}
+                        name ="password"
+                        placeholder="password"
+                        value = {values.password}
+                        onChange={handleChange}
+                        isValid={touched.password && !errors.password}
+                        isInvalid = {!!errors.password}
+                      />
+                      <InputGroup.Prepend>
+                        <div className="col-1 text-center" onClick={toggleShow}>
+                          <FaEye size={32} />
+                        </div>
+                      </InputGroup.Prepend>
+                      <Form.Control.Feedback as={Col} md="12" type="invalid">
+                        Please provide a password.
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
+                  <Form.Group
+                    as={Col}
+                    md="12"
+                    controlId="validationConfirmPassword"
+                  >
+                    <Form.Label>Confirm Password</Form.Label>
+                    <InputGroup>
+                      <Form.Control
+                        required
+                        type={showPasswordConfirm ? "text" : "password"}
+                        name ="passwordConfirm"
+                        placeholder="confirm password"
+                        value = {values.passwordConfirm}
+                        onChange={handleChange}
+                        isValid={touched.passwordConfirm && !errors.passwordConfirm}
+                        isInvalid = {!!errors.passwordConfirm}
+                      />
+                      <InputGroup.Prepend>
+                        <div
+                          className="col-1 text-center"
+                          onClick={toggleShowConfirm}
+                        >
+                          <FaEye size={32} />
+                        </div>
+                      </InputGroup.Prepend>
+                      <Form.Control.Feedback as={Col} md="12" type="invalid">
+                        Please provide a password.
+                      </Form.Control.Feedback>
+                    </InputGroup>
+                  </Form.Group>
+                </Form.Row>
+                <Form.Group>
+                  <Form.Check
+                    type="checkbox"
+                    required
+                    name="terms"
+                    isInvalid = {!!errors.terms}
+                    feedback="You must agree before submitting."
+                    onChange={handleChange}
+                    label={
+                      <React.Fragment>
+                        I accept the {"\u00a0"}
+                        <Link className="link" to="#">
+                          Terms of Use
+                        </Link>
+                        {"\u00a0"} & {"\u00a0"}
+                        <Link className="link" to="#">
+                          Privacy Policy
+                        </Link>
+                        .
+                      </React.Fragment>
+                    }
+                  ></Form.Check>
+                </Form.Group>
+                <Button type="submit">Submit form</Button>
+              </Form>
+            </div>
+          </div>
+        </div>
+      )}
+    </Formik>
   );
 
   return (
