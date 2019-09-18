0a89863f8a52d9c3402af0f6e3e4e29faf2980c6
Arturo Corona <zkdnaem@gmail.com>
2019-09-18 12:45:42 -0500

    Added links to the modal, issues with CSS

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -4,6 +4,7 @@ import "./Modal.css";
 import Card from "react-bootstrap/Card";
 import { connect } from "react-redux";
 import { isAuthenticated } from "../../utils/auth";
+import { Link } from "react-router-dom";
 
 const Modal = ({ show, modalOut }) => {
   var logged = isAuthenticated();
@@ -12,11 +13,19 @@ const Modal = ({ show, modalOut }) => {
     if (show)
       return (
         <Card>
+          {/* Para ir al account que no tenemos */}
+          <Link to={`/esta_pagina_sera_account_details`}>
+            <ModalButton
+              label="Account Details"
+              className="buttonOne"
+            ></ModalButton>
+          </Link>
+
           <ModalButton
-            label="Account Details"
-            className="buttonOne"
+            label="Logout"
+            className="buttonTwo"
+            onClick={this.logout()}
           ></ModalButton>
-          <ModalButton label="Logout" className="buttonTwo"></ModalButton>
         </Card>
       );
     return <div />;
@@ -24,8 +33,12 @@ const Modal = ({ show, modalOut }) => {
     if (show)
       return (
         <Card>
-          <ModalButton label="Login" className="buttonOne"></ModalButton>
-          <ModalButton label="Sign Up" className="buttonTwo"></ModalButton>
+          <Link to={`/signin`}>
+            <ModalButton label="Login" className="buttonOne"></ModalButton>
+          </Link>
+          <Link to={"/signup"}>
+            <ModalButton label="Sign Up" className="buttonTwo"></ModalButton>
+          </Link>
         </Card>
       );
     return <div />;
@@ -41,6 +54,11 @@ function mapStateToProps(state) {
 
 function mapDispatchToProps(dispatch) {
   return {};
+}
+
+function logout() {
+  localStorage.clear();
+  return <Link className="link" to="/"></Link>;
 }
 
 export default connect(
