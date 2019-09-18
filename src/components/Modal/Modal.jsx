10dcc1fcf4f5c07a172c6e942473ba46790d79f8
Arturo Corona <zkdnaem@gmail.com>
2019-09-18 14:49:42 -0500

    Conflicts with modal onClick functions solved

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -24,7 +24,7 @@ const Modal = ({ show, modalOut }) => {
           <ModalButton
             label="Logout"
             className="buttonTwo"
-            onClick={this.logout()}
+            onClick={logout()}
           ></ModalButton>
         </Card>
       );
