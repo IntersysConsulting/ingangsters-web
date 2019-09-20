b7b97fc30c1e4fc02dfe0a7f94a6b49b7d7b3baa
Jorge Garcia <jogadev@outlook.com>
2019-09-20 13:25:21 -0500

    Added image upload process

---
+++ src/components/Admin/Products/ProductView/Connections.js
@@ -0,0 +1,103 @@
+import axios from "axios";
+import { API } from "../../../../config";
+
+const getToken = () => localStorage.getItem("token");
+
+export const updateProduct = async (data, form) => {
+  const body = {
+    _id: data._id,
+    name: form.target.name.value,
+    description: form.target.description.value,
+    price: parseInt(form.target.price.value),
+    stock: parseInt(form.target.stock.value),
+    image: data.image,
+    shippable: data.shippable,
+    available: data.available
+  };
+  const config = {
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: "Bearer " + getToken()
+    }
+  };
+  const endpoint = `${API}/products/update`;
+  try {
+    const res = await axios.put(endpoint, body, config);
+    if (res.status) {
+      alert("Saved successfully");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const loadProduct = async (id, setData) => {
+  const config = {
+    headers: {
+      "Content-Type": "application/json"
+    }
+  };
+  const endpoint = `${API}/products/single`;
+  const body = JSON.stringify({ _id: id });
+  try {
+    const res = await axios.post(endpoint, body, config);
+    const { status, data } = res;
+    var loadingNow = status !== 200;
+    setData(prev => ({
+      ...data,
+      ...prev,
+      showDelete: true,
+      isLoading: loadingNow,
+      submit: updateProduct,
+      _id: id
+    }));
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const newProduct = async data => {
+  // const config = {
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // };
+  // const endpoint = `${API}/products/create`;
+};
+
+const uploadImage = async image => {
+  const config = {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: "Bearer " + getToken(),
+      Accept: "application/json"
+    }
+  };
+  const imgBody = new FormData();
+  imgBody.append("image", image);
+  const endpoint = `${API}/products/images/upload`;
+  try {
+    const res = await axios.post(endpoint, imgBody, config);
+    return res.data.data;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+};
+
+export const imageChange = setData => async evt => {
+  var file = evt.target.files[0];
+  setData(prev => ({
+    ...prev,
+    disableSave: true,
+    image: "https://media.giphy.com/media/3o7TKtnuHOHHUjR38Y/giphy.gif"
+  }));
+  let results = await uploadImage(file);
+  if (results != null) {
+    setData(prev => ({
+      ...prev,
+      disableSave: false,
+      image: results[0]
+    }));
+  }
+};
