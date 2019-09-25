4c67d4e21bf9ae2d38027996301f79e3d14bc014
Sofia Robles <srobles@intersysconsulting.com>
2019-09-25 12:10:51 -0500

    Login modal Style Responsive

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,20 +2,14 @@ import React from "react";
 import NavBar from "../NavBars/GeneralNavBar/NavBar";
 import Shadow from "../UI/Shadow/Shadow";
 import Products from "./Products/Products";
-import FastLoginButton from "../Checkout/FastLogin/FastLoginButton/FastLoginButton";
+import FastLoginModal from "../Checkout/FastLogin/FastLoginModal/FastLoginModal";
 const Home = () => {
   return (
     <div>
       <NavBar />
-      <FastLoginButton />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <br />
-      <Shadow />
-      <Products />
+      <FastLoginModal />
+      {/* <Shadow />
+      <Products /> */}
     </div>
   );
 };
