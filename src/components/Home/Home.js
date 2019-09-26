81df386fbe8e9f782c8519d500ce1f253334830d
Sergio Rodriguez <arodriguezloza@yahoo.com.mx>
2019-09-26 10:19:03 -0500

    Complete component StepProgressBar

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -4,13 +4,79 @@ import NavBar from "../NavBars/GeneralNavBar/NavBar";
 // import Products from "./Products/Products";
 import StepProgressBar from "../UI/StepProgressBar/StepProgressBar";
 
-const Home = () => {
-  return (
-    <div>
-      <NavBar />
-      <StepProgressBar/>
-    </div>
-  );
-};
+class Home extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      step: 1
+    };
+  }
+
+  dec = () => {
+    let value;
+    if (this.state.step - 1 <= 0) {
+      value = 0;
+    } else {
+      value = this.state.step - 1;
+    }
+    this.setState({
+      step: value,
+    });
+    console.log(value);
+  };
+  inc = () => {
+    let value;
+    if (this.state.step + 1 >= 2) {
+      value = 2;
+    } else {
+      value = this.state.step + 1;
+    }
+    console.log(value);
+    this.setState({
+      step: value
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <div
+          className="container"
+          style={{
+            width: "35%",
+            position: "absolute",
+            top: "35%",
+            left: "10%"
+          }}
+        >
+          <StepProgressBar step={this.state.step}/>
+          <div
+            className="container"
+            style={{
+              width: "25%",
+              position: "absolute",
+              top: "30px",
+              left: "10%"
+            }}
+          >
+            <button onClick={this.dec}>{"<<"} </button>
+          </div>
+          <div
+            className="container"
+            style={{
+              width: "25%",
+              position: "absolute",
+              top: "30px",
+              left: "60%"
+            }}
+          >
+            <button onClick={this.inc}>{">>"} </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
 
 export default Home;
--- src/components/UI/StepProgressBar/StepProgressBar.jsx
+++ src/components/UI/StepProgressBar/StepProgressBar.jsx
@@ -1,15 +1,31 @@
 import React from "react";
 import "react-step-progress-bar/styles.css";
 import { ProgressBar, Step } from "react-step-progress-bar";
-// import logo from "./assets/button-green.png";
+// import logo from "/assets/button-green.png";
 
 class StepProgressBar extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      value: 0,
+      maxStep: 1,
+      step: props.step,
       isMobile: false
     };
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.step !== nextProps.step) {
+      let step;
+      step = (100 / prevState.maxStep) * nextProps.step;
+      if (step >= 100) {
+        step = 100;
+      }
+      return {
+        maxStep: prevState.maxStep,
+        step: step,
+        isMobile: prevState.isMobile
+      };
+    }
+    return null;
   }
   componentDidMount() {
     window.addEventListener("resize", this.resize.bind(this));
@@ -17,175 +33,45 @@ class StepProgressBar extends React.Component {
   }
   resize() {
     let isMobile = window.innerWidth <= 760;
-    if (isMobile !== this.state.isMobile) {
-      this.setState({ isMobile: isMobile });
+    let maxStep;
+    let step;
+    isMobile ? (maxStep = 2) : (maxStep = 1);
+    step = (100 / maxStep) * this.state.step;
+    if (step >= 100) {
+      step = 100;
     }
+    if (isMobile !== this.state.isMobile) {
+      this.setState({ isMobile: isMobile, maxStep: maxStep, step: step });
+    }
+    this.setState({ isMobile: isMobile, maxStep: maxStep, step: step });
   }
 
-  dec = () => {
-    let value;
-    if (this.state.value === 0) {
-      value = 0;
-    } else {
-      if (this.state.isMobile) {
-        value = this.state.value - 50;
-      } else {
-        value = this.state.value - 100;
-      }
-    }
-    this.setState({
-      value: value
-    });
-  };
-  inc = () => {
-    let value;
-    if (this.state.value === 100) {
-      value = 100;
-    } else {
-      if (this.state.isMobile) {
-        value = this.state.value + 50;
-      } else {
-        value = this.state.value + 100;
-      }
-    }
-    this.setState({
-      value: value
-    });
-  };
-
   render() {
-    if (this.state.isMobile) {
-      return (
-        <React.Fragment>
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "25%",
-              left: "40%"
-            }}
-          >
-            <ProgressBar
-              percent={this.state.value}
-              filledBackground="linear-gradient(to right, #6cc04a, #78b55e)"
-            >
-              <Step transition="scale">
-                {({ accomplished }) => (
-                  <img
-                    style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
-                    width="20"
-                    src={'/assets/button-green.png'}
-                  />
-                )}
-              </Step>
-              <Step transition="scale">
-                {({ accomplished }) => (
-                  <img
-                    style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
-                    width="20"
-                    src={'/assets/button-green.png'}
-                  />
-                )}
-              </Step>
-              <Step transition="scale">
-                {({ accomplished }) => (
-                  <img
-                    style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
-                    width="20"
-                    src={'/assets/button-green.png'}
-                  />
-                )}
-              </Step>
-            </ProgressBar>
-          </div>
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "30%",
-              left: "40%"
-            }}
-          >
-            <button onClick={this.dec}>{"<<"} </button>
-          </div>
-
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "30%",
-              left: "60%"
-            }}
-          >
-            <button onClick={this.inc}>{">>"} </button>
-          </div>
-        </React.Fragment>
-      );
-    } else {
-      return (
-        <React.Fragment>
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "25%",
-              left: "40%"
-            }}
-          >
-            <ProgressBar
-              percent={this.state.value}
-              filledBackground="linear-gradient(to right, #6cc04a, #78b55e)"
-            >
-              <Step transition="scale">
-                {({ accomplished }) => (
-                  <img
-                    style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
-                    width="20"
-                    src={'/assets/button-green.png'}
-                  />
-                )}
-              </Step>
-              <Step transition="scale">
-                {({ accomplished }) => (
-                  <img
-                    style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
-                    width="20"
-                    src={'/assets/button-green.png'}
-                  />
-                )}
-              </Step>
-            </ProgressBar>
-          </div>
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "30%",
-              left: "40%"
-            }}
-          >
-            <button onClick={this.dec}>{"<<"} </button>
-          </div>
-
-          <div
-            className="container"
-            style={{
-              width: "25%",
-              position: "absolute",
-              top: "30%",
-              left: "60%"
-            }}
-          >
-            <button onClick={this.inc}>{">>"} </button>
-          </div>
-        </React.Fragment>
+    const steps = [];
+    for (var i = 0; i < this.state.maxStep + 1; i++) {
+      steps.push(
+        <Step transition="scale" key={i}>
+          {({ accomplished }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
+              width="20"
+              src="/assets/button-green.png"
+              alt="step"
+            />
+          )}
+        </Step>
       );
     }
+    return (
+      <React.Fragment>
+        <ProgressBar
+          percent={this.state.step}
+          filledBackground="linear-gradient(to right, #6cc04a, #78b55e)"
+        >
+          {steps}
+        </ProgressBar>
+      </React.Fragment>
+    );
   }
 }
 
