6db474cdfd54adf6b6a2ad49ca72a46e1808983a
Sofia Robles <srobles@intersysconsulting.com>
2019-09-26 12:51:49 -0500

    Add Simple NavBar to Checkout

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,10 @@
 import React from "react";
 import PaymentMethods from "./PaymentMethods/PaymentMethods";
-
+import SimpleNavBar from "../NavBars/SimpleNavBar/SimpleNavBar";
 const Checkout = () => {
   return (
     <div>
+      <SimpleNavBar />
       <h1>Checkout</h1>
       <PaymentMethods />
     </div>
