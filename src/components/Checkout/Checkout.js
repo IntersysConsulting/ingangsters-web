d1b38a31a2a6dff4534040475d31020c41dc2cd1
Sofia Robles <srobles@intersysconsulting.com>
2019-09-27 09:28:54 -0500

    Ready for pull request

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,12 +1,11 @@
 import React from "react";
-import PaymentMethods from "./PaymentMethods/PaymentMethods";
 import SimpleNavBar from "../NavBars/SimpleNavBar/SimpleNavBar";
+
 const Checkout = () => {
   return (
     <div>
       <SimpleNavBar />
       <h1>Checkout</h1>
-      <PaymentMethods />
     </div>
   );
 };
