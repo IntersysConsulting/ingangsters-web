f2cf80a24f225c6c1b9aacd208be2bfa02bf3e2b
Martin Hernandez <mhernandez@intersysconsulting.com>
2019-09-30 11:02:29 -0500

    Hotfix AdminRoute
    Now when you enter an AdminRoute (eg. /admin/dashboard) it show the error page (if you are not an admin)

--- src/components/NavBars/SimpleNavBar/SimpleNavBar.js
+++ src/components/NavBars/SimpleNavBar/SimpleNavBar.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import Navbar from "react-bootstrap/Navbar";
 import NavbarBrand from "react-bootstrap/NavbarBrand";
-import { Link } from "react-router-dom";
 import "./SimpleNavBar.css";
 import "../../../css/colors.css";
 
