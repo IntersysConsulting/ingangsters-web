70897e400ef509920cf9ca05974700a98c3b746d
Sofia Robles <srobles@intersysconsulting.com>
2019-10-03 13:03:45 -0500

    AdminTabBar test Redux Actions

--- src/components/Admin/AdminTabBar/AdminTabBar.jsx
+++ src/components/Admin/AdminTabBar/AdminTabBar.jsx
@@ -11,9 +11,10 @@ const AdminTabBar = ({ handleClick }) => {
   return (
     <div className="AdminTabBar">
       <ButtonGroup className="buttonGroupAdmin" size="lg">
-        <div id="adminTabBarUsersButton">
+        <div id="adminTabBarUsersDiv">
           <Link to="/admin/dashboard">
             <Button
+              id="adminTabBarUsersButton"
               className="tabBarFirstButton"
               onClick={handleClick}
               value="Users"
@@ -23,17 +24,22 @@ const AdminTabBar = ({ handleClick }) => {
           </Link>
         </div>
 
-        <div id="adminTabBarProductsButton">
+        <div id="adminTabBarProductsDiv">
           <Link to="/admin/dashboard">
-            <Button onClick={handleClick} value="Products">
+            <Button
+              id="adminTabBarProductsButton"
+              onClick={handleClick}
+              value="Products"
+            >
               Products
             </Button>
           </Link>
         </div>
 
-        <div id="adminTabBarOrdersButton">
+        <div id="adminTabBarOrdersDiv">
           <Link to="/admin/dashboard">
             <Button
+              id="adminTabBarOrdersButton"
               className="tabBarLastButton"
               onClick={handleClick}
               value="Orders"
@@ -59,7 +65,6 @@ function mapDispatchToProps(dispatch) {
   return {
     handleClick(e) {
       dispatch(setActiveAdminBarButton(e.target.value));
-      console.log("State Changed");
     }
   };
 }
--- src/components/Admin/AdminTabBar/AdminTabBar.test.jsx
+++ src/components/Admin/AdminTabBar/AdminTabBar.test.jsx
@@ -3,32 +3,115 @@ import { shallow, mount } from "enzyme";
 import AdminTabBar from "./AdminTabBar";
 import { MemoryRouter } from "react-router-dom";
 import configureStore from "redux-mock-store";
+import { setActiveAdminBarButton } from "../../../actions/creators/AdminTabBar";
+import * as types from "../../../actions/types/AdminTabBar";
+import reducer from "../../../reducers/AdminTabBar";
+import configureMockStore from "redux-mock-store";
+const mockStore = configureMockStore();
+import thunk from "redux-thunk";
 
 describe("AdminTabBar", () => {
-  const fakeStore = configureStore()({
+  const initialState = {
     adminTabBar: {
       activeButton: "Users"
     }
-  });
-
+  };
+  const middlewares = [thunk];
+  const mockStore = configureStore(middlewares);
+  const actions = [setActiveAdminBarButton];
+  const store = mockStore(initialState, actions);
   it("renders correctly", () => {
-    const wrapper = shallow(<AdminTabBar store={fakeStore} dispatch={null} />);
+    const wrapper = shallow(<AdminTabBar store={store} dispatch={null} />);
     expect(wrapper).toMatchSnapshot();
   });
 
   it("Buttons redirect to admin/dashboard", () => {
     const wrapper = mount(
       <MemoryRouter>
-        <AdminTabBar store={fakeStore} dispatch={null} />
+        <AdminTabBar store={store} dispatch={null} />
       </MemoryRouter>
     );
-    let linkElement = wrapper.find("#adminTabBarUsersButton");
+    let linkElement = wrapper.find("#adminTabBarUsersDiv");
     expect(linkElement.find("Link").prop("to")).toBe("/admin/dashboard");
 
-    linkElement = wrapper.find("#adminTabBarProductsButton");
+    linkElement = wrapper.find("#adminTabBarProductsDiv");
     expect(linkElement.find("Link").prop("to")).toBe("/admin/dashboard");
 
-    linkElement = wrapper.find("#adminTabBarOrdersButton");
+    linkElement = wrapper.find("#adminTabBarOrdersDiv");
     expect(linkElement.find("Link").prop("to")).toBe("/admin/dashboard");
+  });
+
+  it("setActiveAdminBarButton Action Creator", () => {
+    const newButton = "Users";
+    const expectedAction = {
+      type: types.SET_ACTIVE_ADMINBAR_BUTTON,
+      newButton
+    };
+    expect(setActiveAdminBarButton(newButton)).toEqual(expectedAction);
+  });
+
+  it("should handle SET_ACTIVE_ADMINBAR_BUTTON", () => {
+    expect(
+      reducer([], {
+        type: types.SET_ACTIVE_ADMINBAR_BUTTON,
+        newButton: "Users"
+      })
+    ).toEqual({
+      activeButton: "Users"
+    });
+
+    expect(
+      reducer([], {
+        type: types.SET_ACTIVE_ADMINBAR_BUTTON,
+        newButton: "Products"
+      })
+    ).toEqual({
+      activeButton: "Products"
+    });
+
+    expect(
+      reducer([], {
+        type: types.SET_ACTIVE_ADMINBAR_BUTTON,
+        newButton: "Orders"
+      })
+    ).toEqual({
+      activeButton: "Orders"
+    });
+  });
+
+  it("handles Click", () => {
+    const wrapper = mount(
+      <MemoryRouter>
+        <AdminTabBar store={store} dispatch={null} />
+      </MemoryRouter>
+    );
+    let event = { target: { value: "Users" } };
+    wrapper
+      .find("#adminTabBarUsersButton")
+      .at(0)
+      .simulate("click", event);
+    expect(store.getActions()).toEqual([
+      { newButton: "Users", type: types.SET_ACTIVE_ADMINBAR_BUTTON }
+    ]);
+
+    event = { target: { value: "Products" } };
+    store.clearActions();
+    wrapper
+      .find("#adminTabBarProductsButton")
+      .at(0)
+      .simulate("click", event);
+    expect(store.getActions()).toEqual([
+      { newButton: "Products", type: types.SET_ACTIVE_ADMINBAR_BUTTON }
+    ]);
+
+    event = { target: { value: "Orders" } };
+    store.clearActions();
+    wrapper
+      .find("#adminTabBarOrdersButton")
+      .at(0)
+      .simulate("click", event);
+    expect(store.getActions()).toEqual([
+      { newButton: "Orders", type: types.SET_ACTIVE_ADMINBAR_BUTTON }
+    ]);
   });
 });
