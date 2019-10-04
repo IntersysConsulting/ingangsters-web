fdd47f86c6af043db459dce5891bd4acebd7ff30
Sofia Robles <srobles@intersysconsulting.com>
2019-10-04 12:01:56 -0500

    Modal makes request for admin/confirm

--- src/components/Modals/ModalPassword/ModalPassword.jsx
+++ src/components/Modals/ModalPassword/ModalPassword.jsx
@@ -4,8 +4,10 @@ import Modal from "react-bootstrap/Modal";
 import RegisterButton from "./RegisterButton/RegisterButton";
 import "../../../css/colors.css";
 import "./ModalPassword.css";
+import { connect } from "react-redux";
+import { checkPassword } from "../../../actions/creators/auth";
 
-function ModalPassword() {
+function ModalPassword({ checkPassword, confirmed_password }) {
   const [values, setValues] = useState({
     password: ""
   });
@@ -15,32 +17,56 @@ function ModalPassword() {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
   const [show, setShow] = React.useState(false);
-  const handleClose = () => setShow(false);
-  const handleShow = () => setShow(true);
+  let handleClose = () => setShow(false);
+  let handleShow = () => setShow(true);
 
+  const onSubmit = async event => {
+    event.preventDefault();
+    checkPassword(password);
+  };
+
+  if (confirmed_password) {
+    alert("Password Confirmed");
+  }
+  if (confirmed_password === false) {
+    alert("Password Incorrect!");
+  }
   return (
     <React.Fragment>
       <RegisterButton onClick={handleShow} />
-      <Modal show={show} onHide={handleClose} className="AskPasswordModal">
-        <Modal.Body className="PasswordModalContent">
-          <form>
-            <label className="PasswordModalLabel">
-              Confirm the register of a New Admin User
-            </label>
-            <div className="password-form-modal">
-              <input
-                onChange={handleChange("password")}
-                type={"password"}
-                placeholder="  Password"
-                className="form-control-password-modal"
-              />
-            </div>
-          </form>
-          <Button className="AskPasswordModalSendButton">Send</Button>
-        </Modal.Body>
-      </Modal>
+      {confirmed_password != true ? (
+        <Modal show={show} onHide={handleClose} className="AskPasswordModal">
+          <Modal.Body className="PasswordModalContent">
+            <form>
+              <label className="PasswordModalLabel">
+                Confirm the register of a New Admin User
+              </label>
+              <div className="password-form-modal">
+                <input
+                  onChange={handleChange("password")}
+                  type={"password"}
+                  placeholder="  Password"
+                  className="form-control-password-modal"
+                />
+              </div>
+            </form>
+            <Button className="AskPasswordModalSendButton" onClick={onSubmit}>
+              Send
+            </Button>
+          </Modal.Body>
+        </Modal>
+      ) : (
+        <div></div>
+      )}
     </React.Fragment>
   );
 }
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticatedl,
+  confirmed_password: state.auth.confirmed_password
+});
 
-export default ModalPassword;
+export default connect(
+  mapStateToProps,
+  { checkPassword }
+)(ModalPassword);
