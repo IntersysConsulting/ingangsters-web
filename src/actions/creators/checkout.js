53ac86068c0e4e4d945ec2cc4ae582ca9d0cc145
Sergio Rodriguez <arodriguezloza@yahoo.com.mx>
2019-10-07 15:42:09 -0500

    Create branch StepProgressBarCheckout

--- src/actions/creators/checkout.js
+++ src/actions/creators/checkout.js
@@ -1,5 +1,21 @@
 import * as types from "../types/checkout";
 
-export function displayPaymentMethods() {
-  return { type: types.DISPLAY_PAYMENT_METHODS };
+export function displayPaymentMethods(value) {
+  return value
+    ? { type: types.DISPLAY_PAYMENT_METHODS }
+    : { type: types.HIDE_PAYMENT_METHODS };
+  //return { type: types.DISPLAY_PAYMENT_METHODS };
 }
+
+export const saveFormValues = (isSaveFormValues, formValues) => {
+  return {
+    type: types.SAVE_FORM_VALUES,
+    isSaveFormValues: isSaveFormValues,
+    formValues: formValues
+  };
+  /* dispatch({
+    type: types.SAVE_FORM_VALUES,
+    isSaveFormValues: isSaveFormValues,
+    formValues: formValues
+  }); */
+};
---
+++ src/components/UI/StepProgressBar/StepProgressBar.jsx
@@ -0,0 +1,87 @@
+import React from "react";
+import "react-step-progress-bar/styles.css";
+import { ProgressBar, Step } from "react-step-progress-bar";
+// import logo from "/assets/button-green.png";
+
+class StepProgressBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      maxStep: 1,
+      step: props.step,
+      isMobile: false
+    };
+  }
+  static getDerivedStateFromProps(nextProps, prevState) {
+    if (prevState.step !== nextProps.step) {
+      let step;
+      step = (100 / prevState.maxStep) * nextProps.step;
+      if (step >= 100) {
+        step = 100;
+      }
+      return {
+        maxStep: prevState.maxStep,
+        step: step,
+        isMobile: prevState.isMobile
+      };
+    }
+    return null;
+  }
+  componentDidMount() {
+    window.addEventListener("resize", this.resize.bind(this));
+    this.resize();
+  }
+  resize() {
+    let isMobile = window.innerWidth <= 767;
+    let maxStep;
+    let step;
+    isMobile ? (maxStep = 2) : (maxStep = 1);
+    step = (100 / maxStep) * this.state.step;
+    if (step >= 100) {
+      step = 100;
+    }
+    if (isMobile !== this.state.isMobile) {
+      this.setState({ isMobile: isMobile, maxStep: maxStep, step: step });
+    }
+    this.setState({ isMobile: isMobile, maxStep: maxStep, step: step });
+  }
+
+  onClickStep = event => {
+    //this.props.setState(event.target.id - 1);
+    console.log(this.props);
+    this.props.setStep(event.target.id - 1);
+    console.log("clic ", event.target.id);
+  };
+
+  render() {
+    const steps = [];
+    for (var i = 0; i < this.state.maxStep + 1; i++) {
+      steps.push(
+        <Step transition="scale" key={i}>
+          {({ accomplished, index }) => (
+            <img
+              style={{ filter: `grayscale(${accomplished ? 0 : 90}%)` }}
+              width="20"
+              src="/assets/button-green.png"
+              alt="step"
+              id={index + 1}
+              onClick={this.onClickStep}
+            />
+          )}
+        </Step>
+      );
+    }
+    return (
+      <React.Fragment>
+        <ProgressBar
+          percent={this.state.step}
+          filledBackground="linear-gradient(to right, #6cc04a, #78b55e)"
+        >
+          {steps}
+        </ProgressBar>
+      </React.Fragment>
+    );
+  }
+}
+
+export default StepProgressBar;
