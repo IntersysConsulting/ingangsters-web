cb78b84112d46a6feb654293c3344bd0b9266cfa
jandreshj <jandreshdzj@gmail.com>
2019-10-09 12:22:55 -0500

    Suits Navbar component's tests with Redux

--- src/components/NavBars/GeneralNavBar/SearchBar/SearchBar.test.jsx
+++ src/components/NavBars/GeneralNavBar/SearchBar/SearchBar.test.jsx
@@ -1,15 +1,28 @@
 import React from "react";
-import { shallow } from "enzyme";
+import { shallow, mount } from "enzyme";
 import SearchBar from "./SearchBar";
+import configureStore from "redux-mock-store";
+import { BrowserRouter as Router } from "react-router-dom";
+import thunk from "redux-thunk";
+
+const middlewares = [thunk];
 
 describe("General Navbar: Search Bar", () => {
   it("renders", () => {
-    const shallowRender = shallow(<SearchBar />);
+    const fakeStore = configureStore(middlewares)();
+    const shallowRender = shallow(
+      <SearchBar store={fakeStore} dispatch={null} />
+    );
     expect(shallowRender).toMatchSnapshot();
   });
 
   it("has both right side variants", () => {
-    const wrapper = shallow(<SearchBar />);
+    const fakeStore = configureStore(middlewares)();
+    const wrapper = mount(
+      <Router>
+        <SearchBar store={fakeStore} dispatch={null} />
+      </Router>
+    );
 
     const searchButtonList = wrapper.find(".append > .searchButton");
     const searchIconList = wrapper.find(".append > .rightSearchIcon");
@@ -19,7 +32,12 @@ describe("General Navbar: Search Bar", () => {
   });
 
   it("prefixIcon", () => {
-    const wrapper = shallow(<SearchBar />);
+    const fakeStore = configureStore(middlewares)();
+    const wrapper = mount(
+      <Router>
+        <SearchBar store={fakeStore} dispatch={null} />
+      </Router>
+    );
 
     const searchIconList = wrapper.find(".searchBar > .prepend");
     expect(searchIconList.children().length).toBe(1);
