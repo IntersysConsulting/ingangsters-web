82c7986c7269e1c90bf63a3794e5821e3856eafa
jandreshj <jandreshdzj@gmail.com>
2019-10-11 13:26:24 -0500

    Adds the filter side bar for admin users

--- src/components/Admin/AdminPage/AdminPage.jsx
+++ src/components/Admin/AdminPage/AdminPage.jsx
@@ -4,27 +4,37 @@ import { connect } from "react-redux";
 import AdminNavBar from "../../NavBars/AdminNavBar/AdminNavBar";
 import AdminProducts from "../Products/index";
 import UsersGrid from "../Users/AdminUsers";
+import SideBar from "../../UI/SideBar/SideBar";
 
 const AdminPage = ({ adminOption }) => {
   return (
-    <div>
+    <div className="sidebar-full-height">
       <AdminNavBar />
-      <AdminTabBar />
-      {adminOption === "Users" ? (
-        <div id="AdminPageContent">
-          <UsersGrid />
+      <div className="container-fluid h-100">
+        <div className="row h-100">
+          <SideBar />
+          <div className="col-sm-12 col-md-8 col-lg-10">
+            <AdminTabBar />
+            {adminOption === "Users" ? (
+              <div id="AdminPageContent">
+                <center>
+                  <p>Users</p>
+                </center>
+              </div>
+            ) : adminOption === "Products" ? (
+              <div id="AdminPageContent">
+                <AdminProducts />
+              </div>
+            ) : (
+              <div id="AdminPageContent">
+                <center>
+                  <p>Orders</p>
+                </center>
+              </div>
+            )}
+          </div>
         </div>
-      ) : adminOption === "Products" ? (
-        <div id="AdminPageContent">
-          <AdminProducts />
-        </div>
-      ) : (
-        <div id="AdminPageContent">
-          <center>
-            <p>Orders</p>
-          </center>
-        </div>
-      )}
+      </div>
     </div>
   );
 };
