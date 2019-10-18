2ed18609b02a0ca61463b2d54be5b7448700ce1b
Arturo Corona <zkdnaem@gmail.com>
2019-10-18 12:10:12 -0500

    Carousel responsive details fixed

--- src/components/UI/Carousel/Carousel.jsx
+++ src/components/UI/Carousel/Carousel.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { Carousel } from "react-bootstrap";
+import "./Carousel.css";
 
 function ControlledCarousel() {
   const [index, setIndex] = useState(0);
@@ -12,42 +13,16 @@ function ControlledCarousel() {
 
   return (
     <Carousel activeIndex={index} direction={direction} onSelect={handleSelect}>
-      {/* <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/assets/imgOne.jpg"
-          alt="First slide"
-        />
-        <Carousel.Caption>
-          <h3>First slide label</h3>
-          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-        </Carousel.Caption>
-      </Carousel.Item> */}
       <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/assets/imgTwo.jpg"
-          alt="Second slide"
-        />
-
-        <Carousel.Caption>
-          <h3>Second slide label</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-        </Carousel.Caption>
+        <div className="img-one-carousel"></div>
       </Carousel.Item>
-      <Carousel.Item>
-        <img
-          className="d-block w-100"
-          src="/assets/imgThree.jpg"
-          alt="Third slide"
-        />
 
-        <Carousel.Caption>
-          <h3>Third slide label</h3>
-          <p>
-            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-          </p>
-        </Carousel.Caption>
+      <Carousel.Item>
+        <div className="img-two-carousel"></div>
+      </Carousel.Item>
+
+      <Carousel.Item>
+        <div className="img-three-carousel"></div>
       </Carousel.Item>
     </Carousel>
   );
